ac0bc9110f308076c28a3da1fc3fd61ecde4ad83
takytank <taki_taki_taky@hotmail.co.jp>
2020-09-24 23:21:32 +0900

    HashMapクラスの追加

--- README.md
+++ README.md
@@ -10,8 +10,12 @@
 
 このライブラリは、以下の記事を参考にして作成されています。
 
+### HashMap
+
+- [C#で競技プログラミングをし続ける人のためのTips](https://qiita.com/Camypaper/items/de6d576fe5513743a50e#dictionarykv)
+
 ### Scanner
 
 - [chokudai さんの tweet](https://twitter.com/chokudai/status/1051741812501536769?s=20)
 - [↑のリンク先](https://atcoder.jp/contests/agc028/submissions/3392785)
-- [C#で競技プログラミングをし続ける人のためのTips](https://qiita.com/Camypaper/items/de6d576fe5513743a50e)
+- [C#で競技プログラミングをし続ける人のためのTips](https://qiita.com/Camypaper/items/de6d576fe5513743a50e#入出力の高速化)
---
+++ TakyTank.KyoProLib.CS8/HashMap.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TakyTank.KyoProLib.CS8
+{
+	public class HashMap<TKey, TValue> : Dictionary<TKey, TValue>
+	{
+		private readonly Func<TKey, TValue> initialzier_;
+		public HashMap(Func<TKey, TValue> initialzier)
+			: base()
+		{
+			initialzier_ = initialzier;
+		}
+
+		public HashMap(Func<TKey, TValue> initialzier, int capacity)
+			: base(capacity)
+		{
+			initialzier_ = initialzier;
+		}
+
+		new public TValue this[TKey key]
+		{
+			get
+			{
+				if (ContainsKey(key) == false) {
+					base[key] = initialzier_(key);
+				}
+
+				return base[key];
+			}
+
+			set { base[key] = value; }
+		}
+	}
+}
