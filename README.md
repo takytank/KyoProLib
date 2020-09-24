7c08aacf3d2037cd911bc266fb784d2c843affe3
takytank <taki_taki_taky@hotmail.co.jp>
2020-09-24 23:14:49 +0900

    Scannerクラスの追加

--- README.md
+++ README.md
@@ -6,3 +6,12 @@
 このライブラリは、MIT License のもとで公開されています。  
 ただし、競技プログラミングのためにこのライブラリの一部をジャッジサーバなどに送信するとき、著作権表示および許諾表示を省略することができます。
 
+## 参考文献
+
+このライブラリは、以下の記事を参考にして作成されています。
+
+### Scanner
+
+- [chokudai さんの tweet](https://twitter.com/chokudai/status/1051741812501536769?s=20)
+- [↑のリンク先](https://atcoder.jp/contests/agc028/submissions/3392785)
+- [C#で競技プログラミングをし続ける人のためのTips](https://qiita.com/Camypaper/items/de6d576fe5513743a50e)
---
+++ TakyTank.KyoProLib.CS8/Scanner.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace TakyTank.KyoProLib.CS8
+{
+	public class Scanner : IDisposable
+	{
+		private const int BUFFER_SIZE = 1024;
+		private const int ASCII_CHAR_BEGIN = 33;
+		private const int ASCII_CHAR_END = 126;
+		private readonly string filePath_;
+		private readonly Stream stream_;
+		private readonly byte[] buf_ = new byte[BUFFER_SIZE];
+		private int length_ = 0;
+		private int index_ = 0;
+		private bool isEof_ = false;
+
+		public Scanner(string file = "")
+		{
+			if (string.IsNullOrWhiteSpace(file)) {
+				stream_ = Console.OpenStandardInput();
+			} else {
+				filePath_ = file;
+				stream_ = new FileStream(file, FileMode.Open);
+			}
+
+			Console.SetOut(new StreamWriter(Console.OpenStandardOutput()) {
+				AutoFlush = false
+			});
+		}
+
+		public void Dispose()
+		{
+			Console.Out.Flush();
+			stream_.Dispose();
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public char Char()
+		{
+			byte b;
+			do {
+				b = Read();
+			} while (b < ASCII_CHAR_BEGIN || ASCII_CHAR_END < b);
+
+			return (char)b;
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public string Next()
+		{
+			var sb = new StringBuilder();
+			for (var b = Char(); b >= ASCII_CHAR_BEGIN && b <= ASCII_CHAR_END; b = (char)Read()) {
+				sb.Append(b);
+			}
+
+			return sb.ToString();
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public int Int() => (int)Long();
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public int Int(int offset) => Int() + offset;
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public (int, int) Int2(int offset = 0)
+			=> (Int(offset), Int(offset));
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public (int, int, int) Int3(int offset = 0)
+			=> (Int(offset), Int(offset), Int(offset));
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public (int, int, int, int) Int4(int offset = 0)
+			=> (Int(offset), Int(offset), Int(offset), Int(offset));
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public int[] ArrayInt(int length, int offset = 0)
+		{
+			int[] Array = new int[length];
+			for (int i = 0; i < length; i++) {
+				Array[i] = Int(offset);
+			}
+			return Array;
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public long Long()
+		{
+			long ret = 0;
+			byte b;
+			bool ng = false;
+			do {
+				b = Read();
+			} while (b != '-' && (b < '0' || '9' < b));
+
+			if (b == '-') {
+				ng = true;
+				b = Read();
+			}
+
+			for (; true; b = Read()) {
+				if (b < '0' || '9' < b) {
+					return ng ? -ret : ret;
+				} else {
+					ret = ret * 10 + b - '0';
+				}
+			}
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public long Long(long offset) => Long() + offset;
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public (long, long) Long2(long offset = 0)
+			=> (Long(offset), Long(offset));
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public (long, long, long) Long3(long offset = 0)
+			=> (Long(offset), Long(offset), Long(offset));
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public (long, long, long, long) Long4(long offset = 0)
+			=> (Long(offset), Long(offset), Long(offset), Long(offset));
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public long[] ArrayLong(int length, long offset = 0)
+		{
+			long[] Array = new long[length];
+			for (int i = 0; i < length; i++) {
+				Array[i] = Long(offset);
+			}
+			return Array;
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public double Double() => double.Parse(Next(), CultureInfo.InvariantCulture);
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public double Double(double offset) => Double() + offset;
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public (double, double) Double2(double offset = 0)
+			=> (Double(offset), Double(offset));
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public (double, double, double) Double3(double offset = 0)
+			=> (Double(offset), Double(offset), Double(offset));
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public (double, double, double, double) Double4(double offset = 0)
+			=> (Double(offset), Double(offset), Double(offset), Double(offset));
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public double[] ArrayDouble(int length, double offset = 0)
+		{
+			double[] Array = new double[length];
+			for (int i = 0; i < length; i++) {
+				Array[i] = Double(offset);
+			}
+			return Array;
+		}
+
+		private byte Read()
+		{
+			if (isEof_) {
+				throw new EndOfStreamException();
+			}
+
+			if (index_ >= length_) {
+				index_ = 0;
+				if ((length_ = stream_.Read(buf_, 0, BUFFER_SIZE)) <= 0) {
+					isEof_ = true;
+					return 0;
+				}
+			}
+
+			return buf_[index_++];
+		}
+
+		public void Save(string text)
+		{
+			if (string.IsNullOrWhiteSpace(filePath_)) {
+				return;
+			}
+
+			File.WriteAllText(filePath_ + "_output.txt", text);
+		}
+	}
+}
