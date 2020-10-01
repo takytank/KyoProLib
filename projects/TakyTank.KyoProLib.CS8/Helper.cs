8837987d51db522703e08d292aca2907068470cd
takytank <taki_taki_taky@hotmail.co.jp>
2020-10-02 01:17:03 +0900

    配列を同じ値で埋める拡張メソッドを追加

--- projects/TakyTank.KyoProLib.CS8/Helper.cs
+++ projects/TakyTank.KyoProLib.CS8/Helper.cs
@@ -33,31 +33,28 @@ namespace TakyTank.KyoProLib.CS8
 		}
 
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public static T[] Array<T>(int n, Func<int, T> init)
-			=> Enumerable.Range(0, n).Select(x => init(x)).ToArray();
+		public static T[] Array<T>(int n, T initialValue) where T : struct
+			=> new T[n].Fill(initialValue);
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public static List<T> List<T>(int n, Func<int, T> init)
-			=> Enumerable.Range(0, n).Select(x => init(x)).ToList();
-
+		public static T[] Array<T>(int n, Func<int, T> initializer)
+			=> Enumerable.Range(0, n).Select(x => initializer(x)).ToArray();
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public static T[,] Array2<T>(int n, int m, T init)
+		public static T[] Fill<T>(this T[] array, T value)
 			where T : struct
 		{
-			var array = new T[n, m];
-			for (int i = 0; i < n; i++) {
-				for (int j = 0; j < m; j++) {
-					array[i, j] = init;
-				}
-			}
-
+			array.AsSpan().Fill(value);
 			return array;
 		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static T[,] Array2<T>(int n, int m, T initialValule) where T : struct
+			=> new T[n, m].Fill(initialValule);
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		public static T[,] Array2<T>(int n, int m, Func<int, int, T> initializer)
 		{
 			var array = new T[n, m];
-			for (int i = 0; i < n; i++) {
-				for (int j = 0; j < m; j++) {
+			for (int i = 0; i < n; ++i) {
+				for (int j = 0; j < m; ++j) {
 					array[i, j] = initializer(i, j);
 				}
 			}
@@ -66,14 +63,30 @@ namespace TakyTank.KyoProLib.CS8
 		}
 
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public static T[,,] Array3<T>(int n1, int n2, int n3, T init)
+		public static T[,] Fill<T>(this T[,] array, T initialValue)
 			where T : struct
 		{
-			var array = new T[n1, n2, n3];
-			for (int i1 = 0; i1 < n1; i1++) {
-				for (int i2 = 0; i2 < n2; i2++) {
-					for (int i3 = 0; i3 < n3; i3++) {
-						array[i1, i2, i3] = init;
+			for (int i = 0; i < array.GetLength(0); ++i) {
+				for (int j = 0; j < array.GetLength(1); ++j) {
+					array[i, j] = initialValue;
+				}
+			}
+
+			return array;
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static T[,,] Array3<T>(int n1, int n2, int n3, T initialValue)
+			where T : struct
+			=> new T[n1, n2, n3].Fill(initialValue);
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static T[,,] Fill<T>(this T[,,] array, T initialValue)
+			where T : struct
+		{
+			for (int i1 = 0; i1 < array.GetLength(0); ++i1) {
+				for (int i2 = 0; i2 < array.GetLength(1); ++i2) {
+					for (int i3 = 0; i3 < array.GetLength(2); ++i3) {
+						array[i1, i2, i3] = initialValue;
 					}
 				}
 			}
@@ -82,15 +95,18 @@ namespace TakyTank.KyoProLib.CS8
 		}
 
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public static T[,,,] Array4<T>(int n1, int n2, int n3, int n4, T init)
+		public static T[,,,] Array4<T>(int n1, int n2, int n3, int n4, T initialValue)
+			where T : struct
+			=> new T[n1, n2, n3, n4].Fill(initialValue);
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static T[,,,] Fill<T>(this T[,,,] array, T initialValue)
 			where T : struct
 		{
-			var array = new T[n1, n2, n3, n4];
-			for (int i1 = 0; i1 < n1; i1++) {
-				for (int i2 = 0; i2 < n2; i2++) {
-					for (int i3 = 0; i3 < n3; i3++) {
-						for (int i4 = 0; i4 < n4; i4++) {
-							array[i1, i2, i3, i4] = init;
+			for (int i1 = 0; i1 < array.GetLength(0); ++i1) {
+				for (int i2 = 0; i2 < array.GetLength(1); ++i2) {
+					for (int i3 = 0; i3 < array.GetLength(2); ++i3) {
+						for (int i4 = 0; i4 < array.GetLength(3); ++i4) {
+							array[i1, i2, i3, i4] = initialValue;
 						}
 					}
 				}
@@ -103,7 +119,7 @@ namespace TakyTank.KyoProLib.CS8
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		public static void DoAt4(int i, int j, int imax, int jmax, Action<int, int> action)
 		{
-			for (int dn = 0; dn < 4; dn++) {
+			for (int dn = 0; dn < 4; ++dn) {
 				int d4i = i + delta4_[dn];
 				int d4j = j + delta4_[dn + 1];
 				if ((uint)d4i < (uint)imax && (uint)d4j < (uint)jmax) {
@@ -116,7 +132,7 @@ namespace TakyTank.KyoProLib.CS8
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		public static void DoAt8(int i, int j, int imax, int jmax, Action<int, int> action)
 		{
-			for (int dn = 0; dn < 8; dn++) {
+			for (int dn = 0; dn < 8; ++dn) {
 				int d8i = i + delta8_[dn];
 				int d8j = j + delta8_[dn + 1];
 				if ((uint)d8i < (uint)imax && (uint)d8j < (uint)jmax) {
@@ -128,7 +144,7 @@ namespace TakyTank.KyoProLib.CS8
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		public static void ForEachSubBit(int bit, Action<int> action)
 		{
-			for (int sub = bit; sub >= 0; sub--) {
+			for (int sub = bit; sub >= 0; --sub) {
 				sub &= bit;
 				action(sub);
 			}
@@ -138,7 +154,7 @@ namespace TakyTank.KyoProLib.CS8
 		public static string Reverse(string src)
 		{
 			var chars = src.ToCharArray();
-			for (int i = 0, j = chars.Length - 1; i < j; i++, j--) {
+			for (int i = 0, j = chars.Length - 1; i < j; ++i, --j) {
 				var tmp = chars[i];
 				chars[i] = chars[j];
 				chars[j] = tmp;
