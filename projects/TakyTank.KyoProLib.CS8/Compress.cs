727bc88c5c5eb15b2f837e546b2e108bafe703af
takytank <taki_taki_taky@hotmail.co.jp>
2020-10-03 19:41:09 +0900

    2次元領域の座標圧縮クラスを追加

--- projects/TakyTank.KyoProLib.CS8/Compress.cs
+++ projects/TakyTank.KyoProLib.CS8/Compress.cs
@@ -53,10 +53,12 @@ namespace TakyTank.KyoProLib.CS8
 		private readonly long[] values_;
 		private readonly Dictionary<long, int> map_;
 
+		public int Length => values_.Length;
+
 		public Compress1D(ReadOnlySpan<(long start, long end)> raws, int offset = 0)
 		{
 			int n = raws.Length;
-			var temp = new List<long>(n * 2);
+			var temp = new List<long>(n * 4);
 			for (int i = 0; i < n; i++) {
 				temp.Add(raws[i].start);
 				temp.Add(raws[i].start + 1);
@@ -65,8 +67,9 @@ namespace TakyTank.KyoProLib.CS8
 			}
 
 			values_ = temp.Distinct().OrderBy(x => x).ToArray();
+			map_ = new Dictionary<long, int>(values_.Length);
 			int number = offset;
-			for (int i = 0; i < values_.Length; ++i, ++number) {
+			for (int i = 0; i < values_.Length - 1; ++i, ++number) {
 				map_[values_[i]] = number;
 			}
 		}
@@ -95,6 +98,93 @@ namespace TakyTank.KyoProLib.CS8
 			}
 
 			return ok;
+		}
+	}
+
+	public class Compress2D
+	{
+		private readonly long[] valuesX_;
+		private readonly Dictionary<long, int> mapX_;
+		private readonly long[] valuesY_;
+		private readonly Dictionary<long, int> mapY_;
+
+		public int Height => valuesY_.Length;
+		public int Width => valuesX_.Length;
+
+		public Compress2D(
+			ReadOnlySpan<(long x1, long y1, long x2, long y2)> raws,
+			int offset = 0)
+		{
+			int n = raws.Length;
+			var tempX = new List<long>(n * 4);
+			var tempY = new List<long>(n * 4);
+			for (int i = 0; i < n; i++) {
+				for (int d = 0; d < 2; d++) {
+					tempX.Add(raws[i].x1 + d);
+					tempX.Add(raws[i].x2 + d);
+					tempY.Add(raws[i].y1 + d);
+					tempY.Add(raws[i].y2 + d);
+				}
+			}
+
+			valuesX_ = tempX.Distinct().OrderBy(x => x).ToArray();
+			valuesY_ = tempX.Distinct().OrderBy(x => x).ToArray();
+			int number = offset;
+			for (int i = 0; i < valuesX_.Length - 1; ++i, ++number) {
+				mapX_[valuesX_[i]] = number;
+			}
+
+			number = offset;
+			for (int i = 0; i < valuesY_.Length - 1; ++i, ++number) {
+				mapY_[valuesY_[i]] = number;
+			}
+		}
+
+		public (int xi, int yi) Zip(long x, long y)
+		{
+			return (mapX_[x], mapY_[y]);
+		}
+
+		public (long x, long y) UnZip(int xi, int yi)
+		{
+			return (valuesX_[xi], valuesY_[yi]);
+		}
+
+		public (int xi, int yi) LowerBound(long x, long y)
+		{
+			int xi;
+			{
+				int ng = -1;
+				int ok = valuesX_.Length - 1;
+				while (ok - ng > 1) {
+					int mid = (ok + ng) / 2;
+					if (valuesX_[mid] >= x) {
+						ok = mid;
+					} else {
+						ng = mid;
+					}
+				}
+
+				xi = ok;
+			}
+
+			int yi;
+			{
+				int ng = -1;
+				int ok = valuesY_.Length - 1;
+				while (ok - ng > 1) {
+					int mid = (ok + ng) / 2;
+					if (valuesY_[mid] >= y) {
+						ok = mid;
+					} else {
+						ng = mid;
+					}
+				}
+
+				yi = ok;
+			}
+
+			return (xi, yi);
 		}
 	}
 }
