2cca9d0e33567da5a01f13860a42fdc9ee0a6c5d
takytank <taki_taki_taky@hotmail.co.jp>
2020-12-23 01:15:01 +0900

    RadixHeap32 に Peek 関数を追加

--- projects/TakyTank.KyoProLib.CSharp.Core31/RadixHeap.cs
+++ projects/TakyTank.KyoProLib.CSharp.Core31/RadixHeap.cs
@@ -10,44 +10,38 @@ namespace TakyTank.KyoProLib.CSharp.Core31
 	public class RadixHeap32<T>
 	{
 		private const int MAX_BIT = 32;
-		private readonly List<(uint priority, T value)>[] buckets_
-			= new List<(uint priority, T value)>[MAX_BIT + 1];
-		private uint lastPriority_;
-
-		public RadixHeap32()
-		{
-			for (int i = 0; i < MAX_BIT + 1; i++) {
-				buckets_[i] = new List<(uint priority, T value)>();
-			}
-		}
+		private readonly List<(uint key, T value)>[] buckets_;
+		private readonly uint[] mins_;
+		private uint lastKey_;
 
 		public int Count { get; private set; }
 
-		public void Push(uint priority, T value)
+		public RadixHeap32()
 		{
-			++Count;
-			buckets_[GetBit(priority ^ lastPriority_)].Add((priority, value));
+			buckets_ = new List<(uint key, T value)>[MAX_BIT + 1];
+			for (int i = 0; i < MAX_BIT + 1; ++i) {
+				buckets_[i] = new List<(uint key, T value)>();
+			}
+
+			mins_ = new uint[MAX_BIT + 1];
+			mins_.AsSpan().Fill(uint.MaxValue);
 		}
 
-		public (uint priority, T value) Pop()
+		public void Push(uint key, T value)
+		{
+			++Count;
+			int bit = GetBit(key ^ lastKey_);
+			buckets_[bit].Add((key, value));
+			mins_[bit] = Math.Min(mins_[bit], key);
+		}
+
+		public (uint key, T value) Pop()
 		{
 			if (Count == 0) {
-				return (0, default);
+				throw new Exception("heap is empty.");
 			}
 
-			if (buckets_[0].Count == 0) {
-				int index = 1;
-				while (buckets_[index].Count == 0) {
-					++index;
-				}
-
-				lastPriority_ = buckets_[index].Min(x => x.priority);
-				foreach (var p in buckets_[index]) {
-					buckets_[GetBit(p.priority ^ lastPriority_)].Add(p);
-				}
-
-				buckets_[index].Clear();
-			}
+			Pull();
 
 			--Count;
 			var ret = buckets_[0][^1];
@@ -55,8 +49,39 @@ namespace TakyTank.KyoProLib.CSharp.Core31
 			return ret;
 		}
 
+		public (uint key, T value) Peek()
+		{
+			if (Count == 0) {
+				throw new Exception("heap is empty.");
+			}
+
+			Pull();
+			return buckets_[0][^1];
+		}
+
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		private int GetBit(uint a)
-			=> a != 0 ? MAX_BIT - BitOperations.LeadingZeroCount(a) : 0;
+		private void Pull()
+		{
+			if (buckets_[0].Count == 0) {
+				int index = 1;
+				while (buckets_[index].Count == 0) {
+					++index;
+				}
+
+				lastKey_ = mins_[index];
+				foreach (var item in buckets_[index]) {
+					int bit = GetBit(item.key ^ lastKey_);
+					buckets_[bit].Add(item);
+					mins_[bit] = Math.Min(mins_[bit], item.key);
+				}
+
+				buckets_[index].Clear();
+				mins_[index] = uint.MaxValue;
+			}
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		private int GetBit(uint x)
+			=> x != 0 ? MAX_BIT - BitOperations.LeadingZeroCount(x) : 0;
 	}
 }
