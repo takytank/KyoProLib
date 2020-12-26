78b056b72e2dd4d07f3c3454e917292da19c03c6
takytank <taki_taki_taky@hotmail.co.jp>
2020-12-26 23:09:02 +0900

    stringのswapを行う拡張メソッドを追加

--- projects/TakyTank.KyoProLib.CSharp.Core31/Extensions.cs
+++ projects/TakyTank.KyoProLib.CSharp.Core31/Extensions.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Numerics;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using System.Text;
 
 namespace TakyTank.KyoProLib.CSharp.Core31
@@ -21,6 +22,29 @@ namespace TakyTank.KyoProLib.CSharp.Core31
 		private class FakeList<T>
 		{
 			public T[] Array = null;
+		}
+
+		public static void Swap(this string str, int i, int j)
+		{
+			var span = str.AsWriteableSpan();
+			(span[i], span[j]) = (span[j], span[i]);
+		}
+
+		public static char Replace(this string str, int index, char c)
+		{
+			var span = str.AsWriteableSpan();
+			char old = span[index];
+			span[index] = c;
+			return old;
+		}
+
+		public static Span<char> AsWriteableSpan(this string str)
+		{
+			unsafe {
+				fixed (char* p = str) {
+					return MemoryMarshal.CreateSpan(ref *p, str.Length);
+				}
+			}
 		}
 	}
 }
