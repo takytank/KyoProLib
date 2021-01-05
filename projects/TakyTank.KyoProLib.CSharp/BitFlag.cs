9ebb14f0e6439ba50a061514fb68de6034e124be
takytank <taki_taki_taky@hotmail.co.jp>
2021-01-06 01:33:16 +0900

    BitFlag.ForEachSubBits に自身と空集合を含まないように変更

--- projects/TakyTank.KyoProLib.CSharp/BitFlag.cs
+++ projects/TakyTank.KyoProLib.CSharp/BitFlag.cs
@@ -22,6 +22,7 @@ namespace TakyTank.KyoProLib.CSharp
 		public BitFlag OrBit(int bitNumber) => (flags_ | (1 << bitNumber));
 		public BitFlag AndBit(int bitNumber) => (flags_ & (1 << bitNumber));
 		public BitFlag XorBit(int bitNumber) => (flags_ ^ (1 << bitNumber));
+		public BitFlag ComplementOf(BitFlag sub) => flags_ & (~sub.flags_);
 
 		public static BitFlag operator ++(BitFlag src) => new BitFlag(src.flags_ + 1);
 		public static BitFlag operator --(BitFlag src) => new BitFlag(src.flags_ - 1);
@@ -59,8 +60,7 @@ namespace TakyTank.KyoProLib.CSharp
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		public void ForEachSubBits(Action<BitFlag> action)
 		{
-			for (BitFlag sub = flags_; sub >= 0; --sub) {
-				sub &= flags_;
+			for (BitFlag sub = (flags_ - 1) & flags_; sub > 0; sub = --sub & flags_) {
 				action(sub);
 			}
 		}
