bf57a89fe233e1b2c2255a50cabe905504f4705a
takytank <taki_taki_taky@hotmail.co.jp>
2021-02-07 23:45:42 +0900

    Dijkstra用のHeapクラスを分離

--- projects/TakyTank.KyoProLib.CSharp.Csc360/MinCostFlow.cs
+++ projects/TakyTank.KyoProLib.CSharp.Csc360/MinCostFlow.cs
@@ -213,7 +213,7 @@ namespace TakyTank.KyoProLib.CSharp.Csc360
 			long minCost = 0;
 			long f = flowLimit;
 			var edgess = flowedEdges_.AsSpan();
-			var que = new HeapQ();
+			var que = new DijkstraQ();
 			while (f > 0) {
 				distances.AsSpan().Fill(INF);
 				distances[s] = 0;
@@ -338,70 +338,5 @@ namespace TakyTank.KyoProLib.CSharp.Csc360
 				Flow = flow;
 			}
 		};
-
-		private class HeapQ
-		{
-			private (long distance, int v)[] heap_;
-
-			public int Count { get; private set; } = 0;
-			public HeapQ()
-			{
-				heap_ = new (long distance, int v)[1024];
-			}
-
-			public void Enqueue(long distance, int v)
-			{
-				var pair = (distance, v);
-				if (heap_.Length == Count) {
-					var newHeap = new (long distance, int v)[heap_.Length * 2];
-					heap_.CopyTo(newHeap, 0);
-					heap_ = newHeap;
-				}
-
-				heap_[Count] = pair;
-				++Count;
-
-				int c = Count - 1;
-				while (c > 0) {
-					int p = (c - 1) >> 1;
-					if (heap_[p].distance > distance) {
-						heap_[c] = heap_[p];
-						c = p;
-					} else {
-						break;
-					}
-				}
-
-				heap_[c] = pair;
-			}
-
-			public (long distance, int v) Dequeue()
-			{
-				(long distance, int v) ret = heap_[0];
-				int n = Count - 1;
-
-				var item = heap_[n];
-				int p = 0;
-				int c = (p << 1) + 1;
-				while (c < n) {
-					if (c != n - 1 && heap_[c + 1].distance < heap_[c].distance) {
-						++c;
-					}
-
-					if (item.distance > heap_[c].distance) {
-						heap_[p] = heap_[c];
-						p = c;
-						c = (p << 1) + 1;
-					} else {
-						break;
-					}
-				}
-
-				heap_[p] = item;
-				Count--;
-
-				return ret;
-			}
-		}
 	}
 }
---
+++ projects/TakyTank.KyoProLib.CSharp/DijkstraQ.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TakyTank.KyoProLib.CSharp
+{
+	public class DijkstraQ
+	{
+		private (long distance, int v)[] heap_;
+
+		public int Count { get; private set; } = 0;
+		public DijkstraQ()
+		{
+			heap_ = new (long distance, int v)[2];
+		}
+
+		public void Enqueue(long distance, int v)
+		{
+			var pair = (distance, v);
+			if (heap_.Length == Count) {
+				var newHeap = new (long distance, int v)[heap_.Length * 2];
+				heap_.CopyTo(newHeap, 0);
+				heap_ = newHeap;
+			}
+
+			heap_[Count] = pair;
+			++Count;
+
+			int c = Count - 1;
+			while (c > 0) {
+				int p = (c - 1) >> 1;
+				if (heap_[p].distance > distance) {
+					heap_[c] = heap_[p];
+					c = p;
+				} else {
+					break;
+				}
+			}
+
+			heap_[c] = pair;
+		}
+
+		public (long distance, int v) Dequeue()
+		{
+			(long distance, int v) ret = heap_[0];
+			int n = Count - 1;
+
+			var item = heap_[n];
+			int p = 0;
+			int c = (p << 1) + 1;
+			while (c < n) {
+				if (c != n - 1 && heap_[c + 1].distance < heap_[c].distance) {
+					++c;
+				}
+
+				if (item.distance > heap_[c].distance) {
+					heap_[p] = heap_[c];
+					p = c;
+					c = (p << 1) + 1;
+				} else {
+					break;
+				}
+			}
+
+			heap_[p] = item;
+			Count--;
+
+			return ret;
+		}
+	}
+}
