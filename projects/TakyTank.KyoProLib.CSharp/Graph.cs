8e7301e08e4cec88ac31942ea3f66e17f3600864
takytank <taki_taki_taky@hotmail.co.jp>
2021-02-11 02:03:31 +0900

    Graphクラスの追加

---
+++ projects/TakyTank.KyoProLib.CSharp/Graph.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace TakyTank.KyoProLib.CSharp
+{
+	public class Graph
+	{
+		private readonly int n_;
+		private readonly List<(long d, int v)>[] tempEdges_;
+		private readonly (long d, int v)[][] edges_;
+
+		public (long d, int v)[][] Edges => edges_;
+
+		public Graph(int n)
+		{
+			n_ = n;
+			tempEdges_ = new List<(long d, int v)>[n];
+			for (int i = 0; i < n; i++) {
+				tempEdges_[i] = new List<(long d, int v)>();
+			}
+
+			edges_ = new (long d, int v)[n][];
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public void AddEdge(int u, int v, long d) => tempEdges_[u].Add((d, v));
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public void Build()
+		{
+			for (int i = 0; i < n_; i++) {
+				edges_[i] = tempEdges_[i].ToArray();
+			}
+		}
+	}
+}
