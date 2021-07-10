181d67de1076910b6309ee1ae0cf6ddc1ad9599d
takytank <taki_taki_taky@hotmail.co.jp>
2021-07-10 18:42:49 +0900

    Rerootingにedge情報を持つバージョンを追加

--- projects/TakyTank.KyoProLib.CSharp/Rerooting.cs
+++ projects/TakyTank.KyoProLib.CSharp/Rerooting.cs
@@ -7,60 +7,60 @@ namespace TakyTank.KyoProLib.CSharp
 {
 	public class Rerooting<T> where T : struct
 	{
-		private readonly int n_;
-		private readonly Func<T, T, T> merge_;
-		private readonly Func<int, T, T> operateNode_;
-		private readonly T unit_;
-		private readonly List<int>[] tempEdges_;
-		private readonly int[][] edges_;
-		private readonly T[][] dp_;
-		private int[] reverseIndexes_;
+		private readonly int _n;
+		private readonly Func<T, T, T> _mergeChild;
+		private readonly Func<int, T, T> _calculateVertex;
+		private readonly T _unit;
+		private readonly List<int>[] _tempEdges;
+		private readonly int[][] _edges;
+		private readonly T[][] _dp;
+		private int[] _reverseIndexes;
 
 		public Rerooting(
 			int n,
-			Func<T, T, T> merge,
-			Func<int, T, T> operateNode,
+			Func<T, T, T> mergeChild,
+			Func<int, T, T> calculateVertex,
 			T unit)
 		{
-			n_ = n;
-			merge_ = merge;
-			operateNode_ = operateNode;
-			unit_ = unit;
+			_n = n;
+			_mergeChild = mergeChild;
+			_calculateVertex = calculateVertex;
+			_unit = unit;
 
-			tempEdges_ = new List<int>[n];
-			edges_ = new int[n][];
-			dp_ = new T[n][];
-			for (int i = 0; i < tempEdges_.Length; ++i) {
-				tempEdges_[i] = new List<int>();
+			_tempEdges = new List<int>[n];
+			_edges = new int[n][];
+			_dp = new T[n][];
+			for (int i = 0; i < _tempEdges.Length; ++i) {
+				_tempEdges[i] = new List<int>();
 			}
 		}
 
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public void AddEdge((int a, int b) v) => tempEdges_[v.a].Add(v.b);
+		public void AddEdge((int a, int b) v) => _tempEdges[v.a].Add(v.b);
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public void AddEdge(int a, int b) => tempEdges_[a].Add(b);
+		public void AddEdge(int a, int b) => _tempEdges[a].Add(b);
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		public void AddEdge2Way((int a, int b) v) => AddEdge2Way(v.a, v.b);
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		public void AddEdge2Way(int a, int b)
 		{
-			tempEdges_[a].Add(b);
-			tempEdges_[b].Add(a);
+			_tempEdges[a].Add(b);
+			_tempEdges[b].Add(a);
 		}
 
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		public void Build()
 		{
-			for (int i = 0; i < tempEdges_.Length; ++i) {
-				edges_[i] = tempEdges_[i].ToArray();
-				dp_[i] = new T[tempEdges_[i].Count];
+			for (int i = 0; i < _tempEdges.Length; ++i) {
+				_edges[i] = _tempEdges[i].ToArray();
+				_dp[i] = new T[_tempEdges[i].Count];
 			}
 		}
 
 		public T[] Search(int root = 0, bool doseOnlyTreeDp = false)
 		{
-			T[] result = new T[n_];
-			result.AsSpan().Fill(unit_);
+			T[] result = new T[_n];
+			result.AsSpan().Fill(_unit);
 
 			TreeDP(root, result);
 			if (doseOnlyTreeDp == false) {
@@ -72,12 +72,12 @@ namespace TakyTank.KyoProLib.CSharp
 
 		private void TreeDP(int v, T[] result)
 		{
-			var parents = new int[n_];
-			var indexes = new int[n_];
-			reverseIndexes_ = new int[n_];
-			var visited = new bool[n_];
-			var routeStack = new Stack<int>(n_);
-			var tempStack = new Stack<int>(n_);
+			var parents = new int[_n];
+			var indexes = new int[_n];
+			_reverseIndexes = new int[_n];
+			var visited = new bool[_n];
+			var routeStack = new Stack<int>(_n);
+			var tempStack = new Stack<int>(_n);
 			routeStack.Push(v);
 			tempStack.Push(v);
 			visited[v] = true;
@@ -85,10 +85,10 @@ namespace TakyTank.KyoProLib.CSharp
 
 			while (tempStack.Count > 0) {
 				int current = tempStack.Pop();
-				for (int i = 0; i < edges_[current].Length; ++i) {
-					int next = edges_[current][i];
+				for (int i = 0; i < _edges[current].Length; ++i) {
+					int next = _edges[current][i];
 					if (visited[next]) {
-						reverseIndexes_[current] = i;
+						_reverseIndexes[current] = i;
 						continue;
 					}
 
@@ -102,19 +102,19 @@ namespace TakyTank.KyoProLib.CSharp
 
 			while (routeStack.Count > 0) {
 				int target = routeStack.Pop();
-				T accum = unit_;
-				for (int i = 0; i < edges_[target].Length; ++i) {
-					int child = edges_[target][i];
+				T accum = _unit;
+				for (int i = 0; i < _edges[target].Length; ++i) {
+					int child = _edges[target][i];
 					if (child == parents[target]) {
 						continue;
 					}
 
-					accum = merge_(accum, dp_[target][i]);
+					accum = _mergeChild(accum, _dp[target][i]);
 				}
 
-				result[target] = operateNode_(target, accum);
+				result[target] = _calculateVertex(target, accum);
 				if (parents[target] >= 0) {
-					dp_[parents[target]][indexes[target]] = result[target];
+					_dp[parents[target]][indexes[target]] = result[target];
 				}
 			}
 		}
@@ -122,39 +122,212 @@ namespace TakyTank.KyoProLib.CSharp
 		private void Reroot(int root, T[] result)
 		{
 			int max = 0;
-			for (int i = 0; i < n_; ++i) {
-				max = Math.Max(max, edges_[i].Length);
+			for (int i = 0; i < _n; ++i) {
+				max = Math.Max(max, _edges[i].Length);
 			}
 
 			var accumL = new T[max + 1];
 			var accumR = new T[max + 1];
 
-			var q = new Queue<BfsInfo>(n_);
+			var q = new Queue<BfsInfo>(_n);
 			q.Enqueue(new BfsInfo(root, -1));
 			while (q.Count > 0) {
 				var current = q.Dequeue();
-				int count = edges_[current.V].Length;
+				int count = _edges[current.V].Length;
 
-				accumL[0] = unit_;
+				accumL[0] = _unit;
 				for (int i = 0; i < count; ++i) {
-					accumL[i + 1] = merge_(accumL[i], dp_[current.V][i]);
+					accumL[i + 1] = _mergeChild(accumL[i], _dp[current.V][i]);
 				}
 
-				accumR[count] = unit_;
+				accumR[count] = _unit;
 				for (int i = count - 1; i >= 0; i--) {
-					accumR[i] = merge_(accumR[i + 1], dp_[current.V][i]);
+					accumR[i] = _mergeChild(accumR[i + 1], _dp[current.V][i]);
 				}
 
-				result[current.V] = operateNode_(current.V, accumL[count]);
+				result[current.V] = _calculateVertex(current.V, accumL[count]);
 
 				for (int i = 0; i < count; ++i) {
-					int next = edges_[current.V][i];
+					int next = _edges[current.V][i];
 					if (next == current.P) {
 						continue;
 					}
 
-					T reverse = operateNode_(current.V, merge_(accumL[i], accumR[i + 1]));
-					dp_[next][reverseIndexes_[next]] = reverse;
+					T reverse = _calculateVertex(current.V, _mergeChild(accumL[i], accumR[i + 1]));
+					_dp[next][_reverseIndexes[next]] = reverse;
+
+					q.Enqueue(new BfsInfo(next, current.V));
+				}
+			}
+		}
+
+		private struct BfsInfo
+		{
+			public int V { get; set; }
+			public int P { get; set; }
+			public BfsInfo(int v, int p)
+			{
+				V = v;
+				P = p;
+			}
+		}
+	}
+
+	public class Rerooting<T, TEdge>
+		where T : struct
+		where TEdge : struct
+	{
+		private readonly int _n;
+		private readonly Func<T, T, T> _mergeChild;
+		private readonly Func<T, TEdge, T> _foldEdge;
+		private readonly Func<int, T, T> _calculateVertex;
+		private readonly T _unit;
+		private readonly List<(int v, TEdge info)>[] _tempEdges;
+		private readonly (int v, TEdge info)[][] _edges;
+		private readonly T[][] _dp;
+		private int[] _reverseIndexes;
+
+		public Rerooting(
+			int n,
+			Func<T, T, T> mergeChild,
+			Func<T, TEdge, T> foldEdge,
+			Func<int, T, T> calculateVertex,
+			T unit)
+		{
+			_n = n;
+			_mergeChild = mergeChild;
+			_foldEdge = foldEdge;
+			_calculateVertex = calculateVertex;
+			_unit = unit;
+
+			_tempEdges = new List<(int v, TEdge info)>[n];
+			_edges = new (int v, TEdge info)[n][];
+			_dp = new T[n][];
+			for (int i = 0; i < _tempEdges.Length; ++i) {
+				_tempEdges[i] = new List<(int v, TEdge info)>();
+			}
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public void AddEdge((int a, int b, TEdge info) v) => _tempEdges[v.a].Add((v.b, v.info));
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public void AddEdge(int a, int b, TEdge info) => _tempEdges[a].Add((b, info));
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public void AddEdge2Way((int a, int b, TEdge info) v) => AddEdge2Way(v.a, v.b, v.info);
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public void AddEdge2Way(int a, int b, TEdge info)
+		{
+			_tempEdges[a].Add((b, info));
+			_tempEdges[b].Add((a, info));
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public void Build()
+		{
+			for (int i = 0; i < _tempEdges.Length; ++i) {
+				_edges[i] = _tempEdges[i].ToArray();
+				_dp[i] = new T[_tempEdges[i].Count];
+			}
+		}
+
+		public T[] Search(int root = 0, bool doseOnlyTreeDp = false)
+		{
+			T[] result = new T[_n];
+			result.AsSpan().Fill(_unit);
+
+			TreeDP(root, result);
+			if (doseOnlyTreeDp == false) {
+				Reroot(root, result);
+			}
+
+			return result;
+		}
+
+		private void TreeDP(int v, T[] result)
+		{
+			var parents = new int[_n];
+			var indexes = new int[_n];
+			_reverseIndexes = new int[_n];
+			var visited = new bool[_n];
+			var routeStack = new Stack<int>(_n);
+			var tempStack = new Stack<int>(_n);
+			routeStack.Push(v);
+			tempStack.Push(v);
+			visited[v] = true;
+			parents[v] = -1;
+
+			while (tempStack.Count > 0) {
+				int current = tempStack.Pop();
+				for (int i = 0; i < _edges[current].Length; ++i) {
+					var (next, _) = _edges[current][i];
+					if (visited[next]) {
+						_reverseIndexes[current] = i;
+						continue;
+					}
+
+					visited[next] = true;
+					parents[next] = current;
+					indexes[next] = i;
+					routeStack.Push(next);
+					tempStack.Push(next);
+				}
+			}
+
+			while (routeStack.Count > 0) {
+				int target = routeStack.Pop();
+				T accum = _unit;
+				for (int i = 0; i < _edges[target].Length; ++i) {
+					var (child, childInfo) = _edges[target][i];
+					if (child == parents[target]) {
+						continue;
+					}
+
+					accum = _mergeChild(accum, _foldEdge(_dp[target][i], childInfo));
+				}
+
+				result[target] = _calculateVertex(target, accum);
+				if (parents[target] >= 0) {
+					_dp[parents[target]][indexes[target]] = result[target];
+				}
+			}
+		}
+
+		private void Reroot(int root, T[] result)
+		{
+			int max = 0;
+			for (int i = 0; i < _n; ++i) {
+				max = Math.Max(max, _edges[i].Length);
+			}
+
+			var accumL = new T[max + 1];
+			var accumR = new T[max + 1];
+
+			var q = new Queue<BfsInfo>(_n);
+			q.Enqueue(new BfsInfo(root, -1));
+			while (q.Count > 0) {
+				var current = q.Dequeue();
+				int count = _edges[current.V].Length;
+
+				accumL[0] = _unit;
+				for (int i = 0; i < count; ++i) {
+					accumL[i + 1] = _mergeChild(accumL[i], _foldEdge(_dp[current.V][i], _edges[current.V][i].info));
+				}
+
+				accumR[count] = _unit;
+				for (int i = count - 1; i >= 0; i--) {
+					accumR[i] = _mergeChild(accumR[i + 1], _foldEdge(_dp[current.V][i], _edges[current.V][i].info));
+				}
+
+				result[current.V] = _calculateVertex(current.V, accumL[count]);
+
+				for (int i = 0; i < count; ++i) {
+					var (next, nextInfo) = _edges[current.V][i];
+					if (next == current.P) {
+						continue;
+					}
+
+					T reverse = _calculateVertex(current.V, _mergeChild(accumL[i], accumR[i + 1]));
+					_dp[next][_reverseIndexes[next]] = reverse;
 
 					q.Enqueue(new BfsInfo(next, current.V));
 				}
