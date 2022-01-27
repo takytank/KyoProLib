3aabd19295acfd579feca6b1370a8bb610df5440
takytank <taki_taki_taky@hotmail.co.jp>
2022-01-27 21:07:28 +0900

    HashMapにマージテクを使うstatic関数を追加

--- projects/TakyTank.KyoProLib.CSharp/HashMap.cs
+++ projects/TakyTank.KyoProLib.CSharp/HashMap.cs
@@ -6,6 +6,22 @@ namespace TakyTank.KyoProLib.CSharp
 {
 	public class HashMap<TKey, TValue> : Dictionary<TKey, TValue>
 	{
+		public static HashMap<TKey, TValue> Merge(
+			HashMap<TKey, TValue> src1,
+			HashMap<TKey, TValue> src2,
+			Func<TValue, TValue, TValue> mergeValues)
+		{
+			if (src1.Count < src2.Count) {
+				(src1, src2) = (src2, src1);
+			}
+
+			foreach (var key in src2.Keys) {
+				src1[key] = mergeValues(src1[key], src2[key]);
+			}
+
+			return src1;
+		}
+
 		private readonly Func<TKey, TValue> initialzier_;
 		public HashMap(Func<TKey, TValue> initialzier)
 			: base()
