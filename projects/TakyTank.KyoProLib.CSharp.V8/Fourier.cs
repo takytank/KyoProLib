6f484e497feb0791b425835275638d63db0bc33b
takytank <taki_taki_taky@hotmail.co.jp>
2023-06-09 20:18:24 +0900

    Fourie に Match 関数を追加

--- projects/TakyTank.KyoProLib.CSharp.V8/Fourier.cs
+++ projects/TakyTank.KyoProLib.CSharp.V8/Fourier.cs
@@ -16,7 +16,7 @@ namespace TakyTank.KyoProLib.CSharp.V8
 			Span<double> values, bool inverses = false)
 		{
 			var complex = new Complex[values.Length];
-			for (int i = 0; i < complex.Length; i++) {
+			for (int i = 0; i < complex.Length; ++i) {
 				complex[i] = new Complex(values[i], 0);
 			}
 
@@ -32,25 +32,23 @@ namespace TakyTank.KyoProLib.CSharp.V8
 		{
 			int n = values.Length;
 			int h = 0;
-			for (int i = 0; 1 << i < n; i++) {
-				h++;
+			for (int i = 0; 1 << i < n; ++i) {
+				++h;
 			}
 
-			for (int i = 0; i < n; i++) {
+			for (int i = 0; i < n; ++i) {
 				int j = 0;
-				for (int k = 0; k < h; k++) {
+				for (int k = 0; k < h; ++k) {
 					j |= (i >> k & 1) << (h - 1 - k);
 				}
 
 				if (i < j) {
-					var temp = values[i];
-					values[i] = values[j];
-					values[j] = temp;
+					(values[j], values[i]) = (values[i], values[j]);
 				}
 			}
 
 			for (int b = 1; b < n; b *= 2) {
-				for (int j = 0; j < b; j++) {
+				for (int j = 0; j < b; ++j) {
 					var w = Complex.FromPolarCoordinates(
 						1.0,
 						(2 * Math.PI) / (2 * b) * j * (inverses ? 1 : -1));
@@ -64,10 +62,61 @@ namespace TakyTank.KyoProLib.CSharp.V8
 			}
 
 			if (inverses) {
-				for (int i = 0; i < n; i++) values[i] /= n;
+				for (int i = 0; i < n; i++) {
+					values[i] /= n;
+				}
 			}
 
 			return values;
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static Complex[,] Fft2D(
+			Complex[,] a, bool inverses = false, bool extends = false, bool inplaces = true)
+		{
+			int height = a.GetLength(0);
+			int width = a.GetLength(1);
+			if (extends) {
+				height = CeilPow2(height);
+				width = CeilPow2(width);
+				inplaces = false;
+			}
+
+			var ret = a;
+			if (inplaces == false) {
+				ret = new Complex[height, width];
+				for (int y = 0; y < height; ++y) {
+					for (int x = 0; x < width; ++x) {
+						ret[y, x] = a[y, x];
+					}
+				}
+			}
+
+			Complex[] tempW = new Complex[width];
+			for (int y = 0; y < height; ++y) {
+				for (int x = 0; x < width; ++x) {
+					tempW[x] = ret[y, x];
+				}
+
+				CooleyTukey(tempW, inverses);
+				for (int x = 0; x < width; ++x) {
+					ret[y, x] = tempW[x];
+				}
+			}
+
+			Complex[] tempH = new Complex[height];
+			for (int x = 0; x < width; ++x) {
+				for (int y = 0; y < height; ++y) {
+					tempH[y] = ret[y, x];
+				}
+
+				CooleyTukey(tempH, inverses);
+				for (int y = 0; y < height; ++y) {
+					ret[y, x] = tempH[y];
+				}
+			}
+
+			return ret;
 		}
 
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
@@ -187,9 +236,9 @@ namespace TakyTank.KyoProLib.CSharp.V8
 				ret = new ModInt[h, w];
 				int hh = a.GetLength(0);
 				int ww = a.GetLength(1);
-				for (int i = 0; i < hh; i++) {
-					for (int j = 0; j < ww; j++) {
-						ret[i, j] = a[i, j];
+				for (int y = 0; y < hh; y++) {
+					for (int x = 0; x < ww; x++) {
+						ret[y, x] = a[y, x];
 					}
 				}
 			}
@@ -227,9 +276,7 @@ namespace TakyTank.KyoProLib.CSharp.V8
 							}
 						}
 
-						var temp = ret;
-						ret = b;
-						b = temp;
+						(b, ret) = (ret, b);
 					}
 
 					if (inverses) {
@@ -274,9 +321,7 @@ namespace TakyTank.KyoProLib.CSharp.V8
 							}
 						}
 
-						var temp = ret;
-						ret = b;
-						b = temp;
+						(b, ret) = (ret, b);
 					}
 
 					if (inverses) {
@@ -304,13 +349,13 @@ namespace TakyTank.KyoProLib.CSharp.V8
 
 			var fa = FFT(aa);
 			var fb = FFT(bb);
-			for (int i = 0; i < fftLength; i++) {
+			for (int i = 0; i < fftLength; ++i) {
 				fa[i] *= fb[i];
 			}
 
 			var convolved = FFT(fa, true);
 			var result = new double[resultLength];
-			for (int i = 0; i < resultLength; i++) {
+			for (int i = 0; i < resultLength; ++i) {
 				result[i] = convolved[i].Real;
 			}
 
@@ -338,6 +383,141 @@ namespace TakyTank.KyoProLib.CSharp.V8
 			return convolved.Slice(0, resultLength);
 		}
 
+		public static (int delta, double correlation, Complex[] correlations)
+			Match(double[] reference, double[] target)
+		{
+			int rn = reference.Length;
+			int tn = target.Length;
+
+			int resultLength = Math.Max(rn, tn);
+			int fftLength = CeilPow2(resultLength);
+
+			var rr = new Complex[fftLength];
+			for (int i = 0; i < fftLength; ++i) {
+				rr[i] = reference[i % rn];
+			}
+
+			var tt = new Complex[fftLength];
+			for (int i = 0; i < fftLength; ++i) {
+				tt[i] = target[i % tn];
+			}
+
+			var fr = FFT(rr);
+			var ft = FFT(tt);
+			var cross = CrossSpectrum(ft, fr);
+			cross = Normalize(cross);
+
+			var correlations = FFT(cross, inverses: true);
+			double correlation = double.MinValue;
+			int delta = 0;
+			for (int i = 0; i < fftLength; i++) {
+				if (correlations[i].Real > correlation) {
+					correlation = correlations[i].Real;
+					delta = i;
+				}
+			}
+
+			if (delta >= fftLength / 2) {
+				int minus = delta - fftLength;
+				delta = rn + minus;
+			}
+
+			return (delta, correlation, correlations.ToArray());
+		}
+
+		public static (int dx, int dy, double correlation, Complex[,] correlations)
+			Match(double[,] reference, double[,] target)
+		{
+			int rh = reference.GetLength(0);
+			int rw = reference.GetLength(1);
+			int th = target.GetLength(0);
+			int tw = target.GetLength(1);
+
+			int resultHeight = Math.Max(rh, th);
+			int resultWidth = Math.Max(rw, tw);
+			int fftHeight = CeilPow2(resultHeight);
+			int fftWidth = CeilPow2(resultWidth);
+
+			var rr = new Complex[fftHeight, fftWidth];
+			for (int i = 0; i < fftHeight; ++i) {
+				for (int j = 0; j < fftWidth; ++j) {
+					rr[i, j] = reference[i % rh, j % rw];
+				}
+			}
+
+			var tt = new Complex[fftHeight, fftWidth];
+			for (int i = 0; i < fftHeight; ++i) {
+				for (int j = 0; j < fftWidth; ++j) {
+					tt[i, j] = target[i % th, j % rw];
+				}
+			}
+
+			//ApplyHanningWindow(tt, false);
+			//ApplyHanningWindow(rr, false);
+
+			var fr = Fft2D(rr);
+			var ft = Fft2D(tt);
+			var cross = CrossSpectrum(ft, fr);
+			//cross = Normalize(cross);
+
+			var correlations = Fft2D(cross, inverses: true);
+			double correlation = double.MinValue;
+			int dx = 0;
+			int dy = 0;
+			for (int y = 0; y < fftHeight; ++y) {
+				for (int x = 0; x < fftWidth; ++x) {
+					if (correlations[y, x].Real > correlation) {
+						correlation = correlations[y, x].Real;
+						dx = x;
+						dy = y;
+					}
+				}
+			}
+
+			if (dx >= fftWidth / 2) {
+				int minus = dx - fftWidth;
+				dx = rw + minus;
+			}
+
+			if (dy >= fftHeight / 2) {
+				int minus = dy - fftHeight;
+				dy = rh + minus;
+			}
+
+			return (dx, dy, correlation, correlations);
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		private static Span<Complex> Normalize(Span<Complex> values)
+		{
+			for (int i = 0; i < values.Length; ++i) {
+				double re = values[i].Real;
+				double im = values[i].Imaginary;
+				double norm = Math.Sqrt(re * re + im * im);
+				values[i] /= norm;
+			}
+
+			return values;
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		private static Complex[,] Normalize(Complex[,] values)
+		{
+			int height = values.GetLength(0);
+			int width = values.GetLength(1);
+
+			for (int y = 0; y < height; ++y) {
+				for (int x = 0; x < width; ++x) {
+					double re = values[y, x].Real;
+					double im = values[y, x].Imaginary;
+					double norm = Math.Sqrt(re * re + im * im);
+					values[y, x] /= norm;
+				}
+			}
+
+			return values;
+		}
+
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		private static int CeilPow2(int n)
 		{
@@ -347,6 +527,68 @@ namespace TakyTank.KyoProLib.CSharp.V8
 			}
 
 			return pow2;
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		private static Span<Complex> CrossSpectrum(Span<Complex> p, Span<Complex> q)
+		{
+			int n = p.Length;
+			var cross = new Complex[n];
+			for (int i = 0; i < n; ++i) {
+				cross[i] = new Complex(
+					p[i].Real * q[i].Real + p[i].Imaginary * q[i].Imaginary,
+					p[i].Imaginary * q[i].Real - p[i].Real * q[i].Imaginary);
+			}
+
+			return cross;
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		private static Complex[,] CrossSpectrum(Complex[,] p, Complex[,] q)
+		{
+			int h = p.GetLength(0);
+			int w = p.GetLength(1);
+			var cross = new Complex[h, w];
+			for (int i = 0; i < h; ++i) {
+				for (int j = 0; j < w; ++j) {
+					cross[i, j] = new Complex(
+						p[i, j].Real * q[i, j].Real + p[i, j].Imaginary * q[i, j].Imaginary,
+						p[i, j].Imaginary * q[i, j].Real - p[i, j].Real * q[i, j].Imaginary);
+				}
+			}
+
+			return cross;
+		}
+
+		static public void ApplyHanningWindow(Complex[,] values, bool inverses)
+		{
+			int height = values.GetLength(0);
+			int width = values.GetLength(1);
+			double cx = width / 2.0;
+			double cy = height / 2.0;
+
+			for (int y = 0; y < height; ++y) {
+				double dy = Math.Abs(y - cy);
+				for (int x = 0; x < width; ++x) {
+					double dx = Math.Abs(x - cx);
+					double r = Math.Sqrt(dx * dx + dy * dy);
+
+					double weight;
+					if (r < width / 2) {
+						weight = 0.5 + 0.5 * Math.Cos(2 * Math.PI * r / (width - 1));
+					} else {
+						weight = 0;
+					}
+
+					if (inverses) {
+						if (weight != 0) {
+							weight = 1 / weight;
+						}
+					}
+
+					values[y, x] *= weight;
+				}
+			}
 		}
 	}
 }
