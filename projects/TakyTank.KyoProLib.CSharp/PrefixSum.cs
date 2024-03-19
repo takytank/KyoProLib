ac81249a23258c14bf9d7eb3db6e6a4f644edbc7
takytank <taki_taki_taky@hotmail.co.jp>
2024-03-19 20:50:51 +0900

    suffixSum に MaxSum と MinSum を計算する関数を追加

--- projects/TakyTank.KyoProLib.CSharp/PrefixSum.cs
+++ projects/TakyTank.KyoProLib.CSharp/PrefixSum.cs
@@ -158,42 +158,157 @@ namespace TakyTank.KyoProLib.CSharp
 			return (l, r, min);
 		}
 	}
-}
 
-public class SuffixSum
-{
-	private readonly int _n;
-	private readonly long[] _sums;
-
-	public long this[int index]
+	/// <summary>
+	/// 後ろからの累積和を取るクラス
+	/// </summary>
+	public class SuffixSum
 	{
-		set => _sums[index] = value;
-		get => _sums[index];
-	}
+		/// <summary>元の配列の長さ</summary>
+		private readonly int _n;
+		/// <summary>Build前は元の配列の値が、Build後は累積和の値が格納されている。</summary>
+		/// <remarks>
+		/// PrefixSumとは違い、N番目がダミーとなるため、インデックスはズレない。
+		/// 累積後は以下の様になる。
+		/// [0] : [0, N-1]の和
+		/// [1] : [1, N-1]の和
+		/// [2] : [2, N-1]の和
+		/// :
+		/// [N-2] : [N-2, N-1]の和
+		/// [N-1] : [N-1, N-1]の和
+		/// [N] : ダミー(長さ0の区間の和=0に相当)
+		/// </remarks>
+		private readonly long[] _values;
 
-	public SuffixSum(int n)
-	{
-		_n = n;
-		_sums = new long[n + 1];
-	}
-
-	public SuffixSum(long[] values)
-		: this(values.Length)
-	{
-		for (int i = 0; i < _n; i++) {
-			_sums[i] = values[i];
+		public long this[int index]
+		{
+			set => _values[index] = value;
+			get => _values[index];
 		}
-	}
 
-	public void Build()
-	{
-		for (int i = _n - 1; i >= 0; i--) {
-			_sums[i] += _sums[i + 1];
+		/// <summary>
+		/// 配列の長さだけを指定してインスタンスを生成
+		/// </summary>
+		/// <param name="n">配列長</param>
+		public SuffixSum(int n)
+		{
+			_n = n;
+			_values = new long[n + 1];
 		}
-	}
 
-	public long Sum(int l, int r)
-	{
-		return _sums[l] - _sums[r];
+		/// <summary>
+		/// 累積前のインスタンスを指定してインスタンスを生成
+		/// </summary>
+		/// <param name="values">
+		/// 累積したい配列。
+		/// 中身がコピーされるので、累積和を取ったときに、ここで渡した配列自体には影響しない。
+		/// </param>
+		public SuffixSum(long[] values)
+			: this(values.Length)
+		{
+			// 格納インデックスをずらさないでコピー
+			Array.Copy(values, _values, _n);
+		}
+
+		/// <summary>
+		/// 累積和を取る
+		/// </summary>
+		/// <remarks>
+		/// 2回以上呼び出した場合、呼び出す度に累積が行われる。
+		/// </remarks>
+		public void Build()
+		{
+			for (int i = _n - 1; i >= 0; i--) {
+				_values[i] += _values[i + 1];
+			}
+		}
+
+		/// <summary>
+		/// 半開区間[L, R)の区間和をO(1)で計算
+		/// </summary>
+		/// <param name="l">区間の左端インデックス</param>
+		/// <param name="r">区間の右端インデックス</param>
+		/// <returns>区間和</returns>
+		public long Sum(int l, int r)
+		{
+			// 区間[L, N)は [L, R) + [R, N) であるため、[L, R)の和は sum[L, N) - sum[R, N) で計算出来る。 
+			// [L, N)は半開区間なので、その和は[L, N-1]の和と等しくなる。
+			// 配列のインデックスはズレていないので、配列に渡す値はLそのままになる。Rも同様。
+			return _values[l] - _values[r];
+		}
+
+		/// <summary>
+		/// 区間和が最大となる区間と和をO(N)で求める
+		/// </summary>
+		/// <returns>
+		/// l, r -> 和が最大となる半開区間[L, R)
+		/// sum -> 区間和
+		/// </returns>
+		public (int l, int r, long sum) CalculateMaxSum()
+		{
+			// 全ての区間和を計算するとO(N^2)になるが、
+			// 区間の左端を決め打ったときの最適な右端をO(1)で求めることで、全体としてO(N)にする。
+			// Sumの実装から分かるとおり、[R, N)までの和を最小にすれば[L, R)が最大になる。
+			// つまり、右から順番にLを決め打っていくときに、和が最小の位置を保持しつつ計算していけばよい。
+
+			// 何も引かないことで0は達成できるので、初期値は0。
+			long min = 0;
+			int minIndex = -1;
+			int l = 0;
+			int r = 0;
+			long max = long.MinValue;
+			for (int i = _n - 1; i >= 0; --i) {
+				long value = _values[i];
+				if (value - min > max) {
+					max = value - min;
+					l = i;
+					r = minIndex - 1; // 最小の箇所は引かれる部分なので、求めたい区間として有効なのはその1個左から
+				}
+
+				if (min > value) {
+					minIndex = i;
+					min = value;
+				}
+			}
+
+			// 半開区間にするための+1
+			++r;
+
+			return (l, r, max);
+		}
+
+		/// <summary>
+		/// 区間和が最小となる区間と和をO(N)で求める
+		/// </summary>
+		/// <returns>
+		/// l, r -> 和が最小となる半開区間[L, R)
+		/// sum -> 区間和
+		/// </returns>
+		public (int l, int r, long sum) CalculateMinSum()
+		{
+			// CalculateMaxSumと同様に和が最大の位置を保持しつつ計算する。
+			long max = 0;
+			int maxIndex = -1;
+			int l = 0;
+			int r = 0;
+			long min = long.MaxValue;
+			for (int i = _n - 1; i >= 0; --i) {
+				long value = _values[i];
+				if (value - max < min) {
+					min = value - max;
+					l = i;
+					r = maxIndex - 1;
+				}
+
+				if (max < value) {
+					maxIndex = i;
+					max = value;
+				}
+			}
+
+			++r;
+
+			return (l, r, min);
+		}
 	}
 }
