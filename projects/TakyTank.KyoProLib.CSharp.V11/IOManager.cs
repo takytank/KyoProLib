0bab3e2b11f12aa5c32f2f368a2823c8a5de50ce
takytank <taki_taki_taky@hotmail.co.jp>
2024-10-29 23:46:55 +0900

    IOManager クラスを追加

---
+++ projects/TakyTank.KyoProLib.CSharp.V11/IOManager.cs
@@ -0,0 +1,377 @@
+﻿using System.Globalization;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace TakyTank.KyoProLib.CSharp.V11;
+
+public class IOManager : IDisposable
+{
+	private const int BUFFER_SIZE = 1024;
+	private const int ASCII_SPACE = 32;
+	private const int ASCII_CHAR_BEGIN = 33;
+	private const int ASCII_CHAR_END = 126;
+	private readonly byte[] _buf = new byte[BUFFER_SIZE];
+	private readonly bool _isStdIn;
+	private readonly Stream _reader;
+	private readonly bool _isStdOut;
+	private readonly TextWriter _writer;
+	private int _length = 0;
+	private int _index = 0;
+	private bool _isEof = false;
+
+	public IOManager(string inFilePath = "", string outFilePath = "")
+	{
+		// Console.Readline をすると UTF-16 の string 型で読み込まれるが、
+		// 競プロの入力は基本的に ASCII の範囲なので、メモリも時間も倍食ってしまう。
+		// byte[] で入力して手動で変換出来るように、TextReader ではなく Stream を使う。
+		if (string.IsNullOrWhiteSpace(inFilePath)) {
+			_isStdIn = true;
+			_reader = Console.OpenStandardInput();
+		} else {
+			_reader = new FileStream(inFilePath, FileMode.Open);
+		}
+
+		// Console.WriteLine も同様であるのだが、今のところ出力速度が問題になったことがなく、
+		// ファイル出力時のエンコード処理の方が面倒そうなので TextWriter を使う。
+		string outPath = GetOutFilePath(inFilePath, outFilePath);
+		if (string.IsNullOrWhiteSpace(outPath)) {
+			// 毎回 flush すると、10^6 回出力したときに TLE してしまう。
+			// 自動 flush は無効にしておき、Dispose 時に flush するようにする。
+			Console.SetOut(new StreamWriter(Console.OpenStandardOutput()) {
+				AutoFlush = false
+			});
+
+			_isStdOut = true;
+			_writer = Console.Out;
+		} else {
+			_writer = new StreamWriter(outPath);
+		}
+	}
+
+	public void Dispose()
+	{
+		_writer.Flush();
+		// Console.In や Console.Out は Dispose してはいけない。
+		if (_isStdIn == false) {
+			_reader.Dispose();
+		}
+
+		if (_isStdOut == false) {
+			_writer.Dispose();
+		}
+
+		// Dispose パターンは面倒なのでサボり。
+		GC.SuppressFinalize(this);
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public string NextLine()
+	{
+		var sb = new StringBuilder();
+		for (var b = Char(); b >= ASCII_SPACE && b <= ASCII_CHAR_END; b = (char)Read()) {
+			sb.Append(b);
+		}
+
+		return sb.ToString();
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public T[] Repeat<T>(int count, Func<IOManager, T> read)
+	{
+		var array = new T[count];
+		for (int i = 0; i < count; ++i) {
+			// キャプチャーを避けるために自身を引数として渡す。
+			array[i] = read(this);
+		}
+
+		return array;
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public char Char()
+	{
+		byte b;
+		do {
+			b = Read();
+		} while (b < ASCII_CHAR_BEGIN || ASCII_CHAR_END < b);
+
+		return (char)b;
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public string String()
+	{
+		var sb = new StringBuilder();
+		for (var b = Char(); b >= ASCII_CHAR_BEGIN && b <= ASCII_CHAR_END; b = (char)Read()) {
+			sb.Append(b);
+		}
+
+		return sb.ToString();
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public string[] ArrayString(int length)
+	{
+		var array = new string[length];
+		for (int i = 0; i < length; ++i) {
+			array[i] = String();
+		}
+
+		return array;
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public int Int() => (int)Long();
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public int Int(int offset) => Int() + offset;
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public (int, int) Int2(int offset = 0)
+		=> (Int(offset), Int(offset));
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public (int, int, int) Int3(int offset = 0)
+		=> (Int(offset), Int(offset), Int(offset));
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public (int, int, int, int) Int4(int offset = 0)
+		=> (Int(offset), Int(offset), Int(offset), Int(offset));
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public (int, int, int, int, int) Int5(int offset = 0)
+		=> (Int(offset), Int(offset), Int(offset), Int(offset), Int(offset));
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public int[] ArrayInt(int length, int offset = 0)
+	{
+		var array = new int[length];
+		for (int i = 0; i < length; ++i) {
+			array[i] = Int(offset);
+		}
+
+		return array;
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public long Long()
+	{
+		long ret = 0;
+		byte b;
+		bool negative = false;
+		// 空白その他の読み飛ばし。+も読み飛ばしてしまって問題無い。
+		do {
+			b = Read();
+		} while (b != '-' && (b < '0' || '9' < b));
+
+		if (b == '-') {
+			negative = true;
+			b = Read();
+		}
+
+		// 下に一桁ずつ加えていく。
+		for (; true; b = Read()) {
+			if (b < '0' || '9' < b) {
+				return negative ? -ret : ret;
+			} else {
+				ret = ret * 10 + (b - '0');
+			}
+		}
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public long Long(long offset) => Long() + offset;
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public (long, long) Long2(long offset = 0)
+		=> (Long(offset), Long(offset));
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public (long, long, long) Long3(long offset = 0)
+		=> (Long(offset), Long(offset), Long(offset));
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public (long, long, long, long) Long4(long offset = 0)
+		=> (Long(offset), Long(offset), Long(offset), Long(offset));
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public (long, long, long, long, long) Long5(long offset = 0)
+		=> (Long(offset), Long(offset), Long(offset), Long(offset), Long(offset));
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public long[] ArrayLong(int length, long offset = 0)
+	{
+		var array = new long[length];
+		for (int i = 0; i < length; ++i) {
+			array[i] = Long(offset);
+		}
+
+		return array;
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public BigInteger Big() => BigInteger.Parse(String());
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public BigInteger Big(long offset) => Big() + offset;
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public (BigInteger, BigInteger) Big2(long offset = 0)
+		=> (Big(offset), Big(offset));
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public (BigInteger, BigInteger, BigInteger) Big3(long offset = 0)
+		=> (Big(offset), Big(offset), Big(offset));
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public (BigInteger, BigInteger, BigInteger, BigInteger) Big4(long offset = 0)
+		=> (Big(offset), Big(offset), Big(offset), Big(offset));
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public (BigInteger, BigInteger, BigInteger, BigInteger, BigInteger) Big5(long offset = 0)
+		=> (Big(offset), Big(offset), Big(offset), Big(offset), Big(offset));
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public BigInteger[] ArrayBig(int length, long offset = 0)
+	{
+		var array = new BigInteger[length];
+		for (int i = 0; i < length; ++i) {
+			array[i] = Big(offset);
+		}
+
+		return array;
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public double Double() => double.Parse(String(), CultureInfo.InvariantCulture);
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public double Double(double offset) => Double() + offset;
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public (double, double) Double2(double offset = 0)
+		=> (Double(offset), Double(offset));
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public (double, double, double) Double3(double offset = 0)
+		=> (Double(offset), Double(offset), Double(offset));
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public (double, double, double, double) Double4(double offset = 0)
+		=> (Double(offset), Double(offset), Double(offset), Double(offset));
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public (double, double, double, double, double) Double5(double offset = 0)
+		=> (Double(offset), Double(offset), Double(offset), Double(offset), Double(offset));
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public double[] ArrayDouble(int length, double offset = 0)
+	{
+		var array = new double[length];
+		for (int i = 0; i < length; ++i) {
+			array[i] = Double(offset);
+		}
+
+		return array;
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public decimal Decimal() => decimal.Parse(String(), CultureInfo.InvariantCulture);
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public decimal Decimal(decimal offset) => Decimal() + offset;
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public (decimal, decimal) Decimal2(decimal offset = 0)
+		=> (Decimal(offset), Decimal(offset));
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public (decimal, decimal, decimal) Decimal3(decimal offset = 0)
+		=> (Decimal(offset), Decimal(offset), Decimal(offset));
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public (decimal, decimal, decimal, decimal) Decimal4(decimal offset = 0)
+		=> (Decimal(offset), Decimal(offset), Decimal(offset), Decimal(offset));
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public (decimal, decimal, decimal, decimal, decimal) Decimal5(decimal offset = 0)
+		=> (Decimal(offset), Decimal(offset), Decimal(offset), Decimal(offset), Decimal(offset));
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public decimal[] ArrayDecimal(int length, decimal offset = 0)
+	{
+		var array = new decimal[length];
+		for (int i = 0; i < length; ++i) {
+			array[i] = Decimal(offset);
+		}
+
+		return array;
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public void WriteLine() => _writer.WriteLine();
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public void WriteLine(bool value) => _writer.WriteLine(value);
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public void WriteLine(char value) => _writer.WriteLine(value);
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public void WriteLine(decimal value) => _writer.WriteLine(value);
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public void WriteLine(double value) => _writer.WriteLine(value);
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public void WriteLine(float value) => _writer.WriteLine(value);
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public void WriteLine(int value) => _writer.WriteLine(value);
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public void WriteLine(uint value) => _writer.WriteLine(value);
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public void WriteLine(long value) => _writer.WriteLine(value);
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public void WriteLine(ulong value) => _writer.WriteLine(value);
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public void WriteLine(BigInteger value) => _writer.WriteLine(value);
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public void WriteLine(string value) => _writer.WriteLine(value);
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public void Write(bool value) => _writer.Write(value);
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public void Write(char value) => _writer.Write(value);
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public void Write(decimal value) => _writer.Write(value);
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public void Write(double value) => _writer.Write(value);
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public void Write(float value) => _writer.Write(value);
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public void Write(int value) => _writer.Write(value);
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public void Write(uint value) => _writer.Write(value);
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public void Write(long value) => _writer.Write(value);
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public void Write(ulong value) => _writer.Write(value);
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public void Write(BigInteger value) => _writer.Write(value);
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public void Write(string value) => _writer.Write(value);
+
+	private byte Read()
+	{
+		if (_isEof) {
+			throw new EndOfStreamException();
+		}
+
+		if (_index >= _length) {
+			_index = 0;
+			if ((_length = _reader.Read(_buf, 0, BUFFER_SIZE)) <= 0) {
+				_isEof = true;
+				return 0;
+			}
+		}
+
+		return _buf[_index++];
+	}
+
+	private static string GetOutFilePath(string inFilePath, string outFilePath)
+	{
+		if (string.IsNullOrWhiteSpace(outFilePath) == false) {
+			return outFilePath;
+		}
+
+		if (string.IsNullOrWhiteSpace(inFilePath)) {
+			return inFilePath;
+		}
+
+
+		string directory = Path.GetDirectoryName(inFilePath);
+		string title = Path.GetFileNameWithoutExtension(inFilePath);
+		string ext = Path.GetExtension(inFilePath);
+		if (directory.EndsWith(@"in")) {
+			// AHCで配布される tools の in フォルダーの横に out フォルダーを作る
+			directory = directory[..^2] + @"out";
+		}
+
+		// ビジュアライザーの一括読み込みは、1234.txt 又は xxx_1234.txt のような形式に対応している。
+		// 出力フォルダーにかかわらず、出力ファイルであることが分かるようにしておく。
+		title = "out_" + title;
+
+		if (Directory.Exists(directory) == false) {
+			Directory.CreateDirectory(directory);
+		}
+
+		return Path.Combine(directory, title + ext);
+	}
+}
