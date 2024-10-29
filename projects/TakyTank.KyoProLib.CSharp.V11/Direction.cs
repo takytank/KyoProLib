a92a8d08039a85ba23dfd06569b5b780d491fd65
takytank <taki_taki_taky@hotmail.co.jp>
2024-10-29 23:53:51 +0900

    Direction4, Rotation クラスを追加

---
+++ projects/TakyTank.KyoProLib.CSharp.V11/Direction.cs
@@ -0,0 +1,204 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace TakyTank.KyoProLib.CSharp.V11;
+
+[Flags]
+public enum Direction4
+{
+	N = 0,
+	D = 0x01,
+	L = 0x02,
+	U = 0x04,
+	R = 0x08,
+}
+
+public static class Direction4Extensions
+{
+	private static readonly int[] _delta4 = { 1, 0, -1, 0, 1 };
+	private static readonly Direction4[] _delta4Dir
+		= { Direction4.D, Direction4.L, Direction4.U, Direction4.R };
+
+	public static char ToSymbol(this Direction4 dir)
+	{
+		return dir switch {
+			Direction4.N => '.',
+			Direction4.D => 'D',
+			Direction4.L => 'L',
+			Direction4.U => 'U',
+			Direction4.R => 'R',
+			_ => '.',
+		};
+	}
+
+	public static int ToIndex4(this Direction4 dir)
+	{
+		return dir switch {
+			Direction4.N => 0,
+			Direction4.D => 0,
+			Direction4.L => 1,
+			Direction4.U => 2,
+			Direction4.R => 3,
+			_ => 0,
+		};
+	}
+
+	public static (int i, int j) Move(this Direction4 dir, int i, int j)
+	{
+		return dir switch {
+			Direction4.N => (i, j),
+			Direction4.D => (i + 1, j),
+			Direction4.L => (i, j - 1),
+			Direction4.U => (i - 1, j),
+			Direction4.R => (i, j + 1),
+			_ => (i, j),
+		};
+	}
+
+	public static (int i, int j) Move(this Direction4 dir, int i, int j, int d)
+	{
+		return dir switch {
+			Direction4.N => (i, j),
+			Direction4.D => (i + d, j),
+			Direction4.L => (i, j - d),
+			Direction4.U => (i - d, j),
+			Direction4.R => (i, j + d),
+			_ => (i, j),
+		};
+	}
+
+	public static Direction4 Reverse(this Direction4 dir)
+	{
+		return dir switch {
+			Direction4.N => Direction4.N,
+			Direction4.D => Direction4.U,
+			Direction4.L => Direction4.R,
+			Direction4.U => Direction4.D,
+			Direction4.R => Direction4.L,
+			_ => Direction4.N,
+		};
+	}
+
+	public static Direction4 Rorate(this Direction4 dir, Rotation rot)
+	{
+		if (rot == Rotation.N) {
+			return dir;
+		}
+
+		return dir switch {
+			Direction4.N => Direction4.N,
+			Direction4.D => rot == Rotation.L ? Direction4.R : Direction4.L,
+			Direction4.L => rot == Rotation.L ? Direction4.D : Direction4.U,
+			Direction4.U => rot == Rotation.L ? Direction4.L : Direction4.R,
+			Direction4.R => rot == Rotation.L ? Direction4.U : Direction4.D,
+			_ => Direction4.N,
+		};
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static IEnumerable<Direction4> ForEach()
+	{
+		yield return Direction4.N;
+		yield return Direction4.U;
+		yield return Direction4.R;
+		yield return Direction4.D;
+		yield return Direction4.L;
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static IEnumerable<Direction4> ForEach4()
+	{
+		for (int dn = 0; dn < 4; ++dn) {
+			yield return _delta4Dir[dn];
+		}
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static IEnumerable<(int i, int j, Direction4 dir)> Adjacence4(int i, int j, int imax, int jmax)
+	{
+		for (int dn = 0; dn < 4; ++dn) {
+			int d4i = i + _delta4[dn];
+			int d4j = j + _delta4[dn + 1];
+			var dir = _delta4Dir[dn];
+			if ((uint)d4i < (uint)imax && (uint)d4j < (uint)jmax) {
+				yield return (d4i, d4j, dir);
+			}
+		}
+	}
+}
+
+[Flags]
+public enum Rotation
+{
+	N = 0,
+	L = 0x01,
+	R = 0x02,
+}
+
+public static class RotationExtensions
+{
+	public static char ToSymbol(this Rotation rot)
+	{
+		return rot switch {
+			Rotation.N => '.',
+			Rotation.L => 'L',
+			Rotation.R => 'R',
+			_ => '.',
+		};
+	}
+
+	public static (int i, int j) Rotate(this Rotation rot, int i, int j)
+	{
+		return rot switch {
+			Rotation.N => (i, j),
+			Rotation.L => (-j, i),
+			Rotation.R => (j, -i),
+			_ => (i, j),
+		};
+	}
+
+	public static (int i, int j) Rotate(this Rotation rot, int i, int j, int cy, int cx)
+	{
+		int dx = j - cx;
+		int dy = i - cy;
+		switch (rot) {
+			case Rotation.N:
+				return (i, j);
+
+			case Rotation.L: {
+					int jj = dy;
+					int ii = -1 * dx;
+					return (ii + cy, jj + cx);
+				}
+
+			case Rotation.R: {
+					int jj = -1 * dy;
+					int ii = dx;
+					return (ii + cy, jj + cx);
+				}
+
+			default:
+				return (i, j);
+		}
+	}
+
+	public static Rotation GetRotation(Direction4 cur, Direction4 tag)
+	{
+		if (cur == tag || cur == Direction4.N || tag == Direction4.N) {
+			return Rotation.N;
+		}
+
+		int ci = cur.ToIndex4();
+		int ti = tag.ToIndex4();
+		// 真反対の場合はとりあえずどっちでもいい
+		if (Math.Abs(ci - ti) == 2) {
+			return Rotation.L;
+		}
+
+		int cci = ci - 1;
+		if (cci < 0) {
+			cci += 4;
+		}
+
+		return cci == ti ? Rotation.L : Rotation.R;
+	}
+}
