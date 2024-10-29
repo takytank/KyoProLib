44691d70c4187eaeed46ee7d2d951871cb994129
takytank <taki_taki_taky@hotmail.co.jp>
2024-10-30 00:01:29 +0900

    Trace クラスを追加

---
+++ projects/TakyTank.KyoProLib.CSharp.V11/Trace.cs
@@ -0,0 +1,46 @@
+﻿namespace TakyTank.KyoProLib.CSharp.V11;
+
+/// <summary>
+/// 移動経路復元用のクラス
+/// </summary>
+/// <typeparam name="T">移動情報の型</typeparam>
+public class Trace<T>
+{
+	/// <summary>移動情報ログ</summary>
+	/// <remarks>
+	/// IDはこのフィールドのインデックスに対応している。
+	/// 移動元のIDを保持することによって、任意の位置から開始位置に辿ることが可能。
+	/// </remarks>
+	private readonly List<(T move, int prevID)> _log = new() { (default, -1) };
+
+	/// <summary>
+	/// 移動情報の追加
+	/// </summary>
+	/// <param name="move">どういう移動を行ったかの情報</param>
+	/// <param name="prevID">移動元のID</param>
+	/// <returns>移動先のID</returns>
+	public int Add(T move, int prevID)
+	{
+		_log.Add((move, prevID));
+		return _log.Count - 1;
+	}
+
+	/// <summary>
+	/// 移動開始位置から指定したIDに対応する位置までの移動情報を復元する
+	/// </summary>
+	/// <param name="id">移動先の位置に対応するID</param>
+	/// <returns>
+	/// 復元した移動情報。
+	/// stackの上から順番に使用する事でidで指定した位置に辿り着ける。
+	/// </returns>
+	public Stack<T> GetRouteTo(int id)
+	{
+		var route = new Stack<T>();
+		while (id != 0) {
+			route.Push(_log[id].move);
+			id = _log[id].prevID;
+		}
+
+		return route;
+	}
+}
