052380da8c7e3522439bcf12d0c05fe6e25aeaeb
takytank <taki_taki_taky@hotmail.co.jp>
2024-10-30 00:18:30 +0900

    HeuristicHelper クラスを追加

---
+++ projects/TakyTank.KyoProLib.CSharp.V11/HeuristicHelper.cs
@@ -0,0 +1,106 @@
+﻿using System.Diagnostics;
+
+namespace TakyTank.KyoProLib.CSharp.V11;
+
+public class HeuristicHelper
+{
+	public static void RunCases<T>(
+		int runCaseCount,
+		double testCaseCount,
+		bool isParallel,
+		Func<int, T> run,
+		Func<object, int, T, (long score, int loop, int up)> outputCaseInformation,
+		Action<double, int> addOutput = null)
+	{
+#if DEBUG
+		object locker = new();
+		long scoreSum = 0;
+		double scoreLogSum = 0;
+		long loopSum = 0;
+		long upSum = 0;
+		long scoreMin = long.MaxValue;
+		long scoreMax = long.MinValue;
+		int errorCount = 0;
+		if (isParallel) {
+			Parallel.For(0, runCaseCount, i => {
+				RunCases(i);
+			});
+		} else {
+			for (int i = 0; i < runCaseCount; i++) {
+				RunCases(i);
+			}
+		}
+
+		void RunCases(int i)
+		{
+			try {
+				var ret = run(i);
+				var (score, loop, up) = outputCaseInformation(locker, i, ret);
+				Console.Out.Flush();
+				lock (locker) {
+					scoreSum += score;
+					scoreLogSum += Math.Log10(score);
+					loopSum += loop;
+					upSum += up;
+					scoreMin = Math.Min(score, scoreMin);
+					scoreMax = Math.Max(score, scoreMax);
+				}
+			} catch (Exception ex) {
+				lock (locker) {
+					++errorCount;
+					Console.WriteLine($"{i:d4}: {ex.Message}");
+					Console.Out.Flush();
+				}
+			}
+		}
+
+		scoreSum = (long)(scoreSum / (runCaseCount / testCaseCount));
+		scoreLogSum /= runCaseCount / testCaseCount;
+		Console.WriteLine("");
+
+		Console.WriteLine("");
+		Console.WriteLine($"sum: {scoreSum}");
+		Console.WriteLine($"ave: {scoreSum / testCaseCount}");
+		Console.WriteLine($"min: {scoreMin}");
+		Console.WriteLine($"max: {scoreMax}");
+		Console.WriteLine($"log: {scoreLogSum / testCaseCount}");
+		Console.WriteLine($"loop ave.: {loopSum / (double)runCaseCount:f3}");
+		Console.WriteLine($"up ave.: {upSum / (double)runCaseCount:f3}");
+
+		addOutput?.Invoke(testCaseCount, runCaseCount);
+
+		Console.WriteLine($"error : {errorCount}");
+
+		Console.Out.Flush();
+#else
+		run(-1);
+#endif
+	}
+
+	public static (bool isDebug, Stopwatch sw, Random rnd) Initialize()
+	{
+		var sw = new Stopwatch();
+		sw.Start();
+
+		bool isDebug = false;
+#if DEBUG
+		isDebug = true;
+#endif
+		var rnd = new Random();
+
+		return (isDebug, sw, rnd);
+	}
+
+	public static IOManager CreateIO(
+		string caseDirectory, int caseNumber)
+	{
+#if DEBUG
+		var cin = caseNumber >= 0
+			? new IOManager($"{caseDirectory}{caseNumber:d4}.txt")
+			: new IOManager();
+#else
+		var cin = new IOManager();
+#endif
+		return cin;
+	}
+}
